ade47d6f7595a4d2108e8582a945cfacc5c2fe4f
zaki <dzaky@match-advertising.com>
2017-12-20 08:53:30 +0700

    ga

--- application/views/menu/administrator/ga/dash_report.php
+++ application/views/menu/administrator/ga/dash_report.php
@@ -8,7 +8,7 @@
                     <!-- /.col-lg-12 -->
                 </div>
                 <div class="row">
-                    <div class="col-lg-3 col-md-6">
+                   <!--  <div class="col-lg-3 col-md-6">
                         <div class="panel panel-primary">
                             <div class="panel-heading">
                                 <div class="row">
@@ -44,6 +44,29 @@
                                 </div>
                             </div>
                             <a href="<?php echo base_url('administrator/Logistik/print_prc')?>">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div> -->
+
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-print fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge"></div>
+                                        <div>Laporan PO</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="<?php echo base_url('administrator/Genaff/lap_po')?>">
                                 <div class="panel-footer">
                                     <span class="pull-left">View Details</span>
                                     <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
