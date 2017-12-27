d089a4b6df4b4dbbfc7fb26179000d87bc63ef97
kaisha <kaishasatrio@match-advertising.com>
2017-12-27 14:49:34 +0700

    fungsi terbilang

--- application/models/CRUD/M_gen.php
+++ application/models/CRUD/M_gen.php
@@ -14,39 +14,39 @@
 			} 
 			else if ($nilai <20) 
 			{
-				$temp = penyebut($nilai - 10). " belas";
+				$temp = $this->number_conv($nilai - 10). " belas ";
 			} 
 			else if ($nilai < 100) 
 			{
-				$temp = penyebut($nilai/10)." puluh". penyebut($nilai % 10);
+				$temp = $this->number_conv($nilai/10)." puluh ". $this->number_conv($nilai % 10);
 			} 
 			else if ($nilai < 200) 
 			{
-				$temp = " seratus" . penyebut($nilai - 100);
+				$temp = " seratus " . $this->number_conv($nilai - 100);
 			} 
 			else if ($nilai < 1000) 
 			{
-				$temp = penyebut($nilai/100) . " ratus" . penyebut($nilai % 100);
+				$temp = $this->number_conv($nilai/100) . " ratus " . $this->number_conv($nilai % 100);
 			} 
 			else if ($nilai < 2000) 
 			{
-				$temp = " seribu" . penyebut($nilai - 1000);
+				$temp = " seribu " . $this->number_conv($nilai - 1000);
 			} 
 			else if ($nilai < 1000000) 
 			{
-				$temp = penyebut($nilai/1000) . " ribu" . penyebut($nilai % 1000);
+				$temp = $this->number_conv($nilai/1000) . " ribu " . $this->number_conv($nilai % 1000);
 			} 
 			else if ($nilai < 1000000000) 
 			{
-				$temp = penyebut($nilai/1000000) . " juta" . penyebut($nilai % 1000000);
+				$temp = $this->number_conv($nilai/1000000) . " juta " . $this->number_conv($nilai % 1000000);
 			} 
 			else if ($nilai < 1000000000000) 
 			{
-				$temp = penyebut($nilai/1000000000) . " milyar" . penyebut(fmod($nilai,1000000000));
+				$temp = $this->number_conv($nilai/1000000000) . " milyar " . $this->number_conv(fmod($nilai,1000000000));
 			} 
 			else if ($nilai < 1000000000000000) 
 			{
-				$temp = penyebut($nilai/1000000000000) . " trilyun" . penyebut(fmod($nilai,1000000000000));
+				$temp = $this->number_conv($nilai/1000000000000) . " trilyun " . $this->number_conv(fmod($nilai,1000000000000));
 			}
 			if($value<0) 
 			{
