15e79c04f3e7ce3a82d399ab79ed9ec5ac4a1261
kaisha <kaishasatrio@match-advertising.com>
2018-06-11 13:17:05 +0700

    update cetak kas masuk

--- application/views/menu/administrator/finance/fin_/km_print.php
+++ application/views/menu/administrator/finance/fin_/km_print.php
@@ -53,7 +53,7 @@
         }
         .content-font
         {
-            font-family: "Arial";
+            font-family: "times new roman";
             font-size: 15px;
         }
         .foot-font
@@ -105,34 +105,27 @@
     <div class="container">
         <input type="hidden" name="km_id" value="<?php echo $id;?>">
         <input type="hidden" name="cust_id" value="<?php echo $id;?>">
-        <!--<div class="container-fluid"> -->               
-            <!--<hr style="border: solid 2px; color: black; margin-top: 0; margin-bottom: 0;">-->
         <div class="row">
             <div class="col-xs-4">
-                <img class="logo" src="https://www.matchadonline.com/logo_n_watermark/mobile_1481852222932_2logo4.png">
+                <img id="img_logo" class="img-responsive logo" src="">
             </div>
             <div class="col-xs-4 text-center">
                 <h3><strong><u>BUKTI KAS MASUK</u></strong></h3>
                 <h3 style="margin-top:-10px">No.<span name="no_km"></span></h3>
-                <span name="acc_header"></span>
+                <h3 style="margin-top: -10px" name="acc_header"></h3>
             </div>
         </div>
-            <!--<hr>-->
         <div class="row">
             <div class="col-xs-4 head-font">
                 <address>
-                    <!-- <strong>Dari:</strong><br> -->
                     <strong>Match Advertising</strong><br>
                     JL. Lesti No.42, Surabaya 60241<br>
                     Telp. (031) 567 8346 (Hunting)<br>
                     Fax. (031) 568 0646<br>
                     Email : info@match-advertising.com<br>
-                    <!-- Website : www.match-advertising.com<br>
-                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    www.matchadonline.com -->
                 </address>
             </div>
-            <div class="col-xs-4 head-font">
+            <div class="col-xs-5 head-font">
                 <address>
                     <strong>Kepada :</strong><br>
                     <span name="kas_custname"></span><br>
@@ -141,12 +134,9 @@
                     <span name="kas_custinfo"></span>
                 </address>
             </div>
-            <div class="col-xs-4 head-font">
+            <div class="col-xs-3 head-font">
                 <address>
-                    <span>Tanggal :</span>&nbsp;<span name="kas_tgl"></span> 
-                    <!-- <strong>Info:</strong><br> 
-                    Lokasi <span name="loc_name"></span>, <span name="loc_det"></span><br>
-                    <span name="km_info"></span> -->
+                    <strong>Tanggal :</strong>&nbsp;<span name="kas_tgl"></span>
                 </address>
             </div>
         </div>
@@ -159,7 +149,7 @@
                 <table id="tb_km" class="table table-bordered" cellspacing="0" width="100%">
                     <thead>
                         <tr>
-                            <th class="col-sm-2 col-xs-2">Perkiraan</th>
+                            <th class="col-sm-2 col-xs-2 text-center">Perkiraan</th>
                             <th class="col-sm-7 col-xs-7 text-center">Uraian</th>
                             <th class="col-sm-2 col-xs-2 text-center">Jumlah</th>
                         </tr>
@@ -203,8 +193,7 @@
                        (.................)
                 </div>
             </div>
-        </div>        
-        <!--</div>-->
+        </div>
     </div>
     <!-- jQuery -->
     <?php include 'application/views/layout/administrator/jspack.php' ?>
@@ -215,41 +204,10 @@
             id=$('[name="km_id"]').val();            
             prc = 0; qty = 0; sub = 0;
             pick_km(id);
-            
-            // $('[name=po_qty]').on('input', function() {
-                // hitung();
-            // });
+            pick_branch("<?= $this->session->userdata('user_branch')?>");
         });
-
-        function dtable()
-        {
-            //datatables        
-            table = $('#dtb_km').DataTable({
-                "info": false,
-                "destroy": true,
-                "responsive": true,
-                "processing": true, //Feature control the processing indicator.
-                "serverSide": true, //Feature control DataTables' server-side processing mode.
-                "order": [], //Initial no order.
-                // Load data for the table's content from an Ajax source
-                "ajax": {
-                    "url": "<?php echo site_url('administrator/Finance/ajax_printkm')?>",
-                    "type": "POST",                
-                },
-                //Set column definition initialisation properties.
-                "columnDefs": [
-                { 
-                    "targets": [ 0 ], //first column / numbering column
-                    "orderable": false, //set not orderable
-                },
-                ],
-            });
-        }
-
-
         function pick_km(id)
         {
-            //Ajax Load data from ajax
             $.ajax({
                 url : "<?php echo site_url('administrator/Finance/ajax_pick_km/')?>/" + id,
                 type: "GET",
@@ -257,25 +215,16 @@
                 success: function(data)
                 {   
                     $('[name="km_id"]').val(data.CSH_ID);
-                    search_acc(data.COA_ID);
+                    pick_acc(data.COA_ID);
                     $('[name="km_code"]').val(data.CSH_CODE);
                     $('[name="no_km"]').text(data.CSH_CODE);
-                    $('[name="kas_tgl"]').text(data.CSH_DATE);
+                    $('[name="kas_tgl"]').text(moment(data.CSH_DATE).locale('id').format('DD-MMM-YYYY'));
                     $('[name="kas_info"]').val(data.CSH_INFO);
-                    // $('[name="appr_id"]').val(data.APPR_ID);
                     $('[name="cust_id"]').val(data.CUST_ID);
                     $('[name="pass"]').text(data.CSH_ID);
-                    pick_cust($('[name="cust_id"]').val());
+                    (data.CUST_ID != '')?pick_cust(data.CUST_ID):'';
                     pick_sum_total_km($('[name="km_id"]').val());
                     pick_curr(data.CURR_ID);
-                    
-                    // pick_kmdet($('[name="km_id"]').val());
-                    // pick_terbilang_total_km($('[name="kas_total"]').val());                    
-                    // if(data.APPR_ID != null)
-                    // {
-                    //     pick_appr($('[name="appr_id"]').val());
-                    // }
-                    // $('#modal_km').modal('hide');
                 },
                 error: function (jqXHR, textStatus, errorThrown)
                 {
@@ -283,10 +232,8 @@
                 }
             });
         }
-
         function pick_cust(id)
         {
-            //Ajax Load data from ajax
             $.ajax({
                 url : "<?php echo site_url('administrator/Finance/ajax_pick_cust/')?>/" + id,
                 type: "GET",
@@ -297,7 +244,6 @@
                     $('[name="kas_custaddr"]').text(data.CUST_ADDRESS);
                     $('[name="kas_custcity"]').text(data.CUST_CITY);
                     $('[name="kas_custphone"]').text(data.CUST_PHONE);
-                    // $('[name="inv_suppinfo"]').text(data.SUPP_OTHERCTC);
                 },
                 error: function (jqXHR, textStatus, errorThrown)
                 {
@@ -305,12 +251,10 @@
                 }
             });
         }
-
         function pick_sum_total_km(id)
         {
-            //Ajax Load data from ajax
             $.ajax({
-                url : "<?php echo site_url('administrator/Finance/ajax_pick_sum_km/')?>/" + id,
+                url : "<?php echo site_url('administrator/Finance/ajax_pick_sum_km/')?>" + id,
                 type: "GET",
                 dataType: "JSON",
                 success: function(data)
@@ -324,10 +268,8 @@
                 }
             });
         }
-
         function pick_terbilang_total_km(total)
         {
-            //Ajax Load data from ajax
             $.ajax({
                 url : "<?php echo site_url('administrator/Finance/get_numbsp/')?>" + total,
                 type: "GET",
@@ -343,12 +285,10 @@
                 }
             });
         }
-
-       function pick_kmdet(id)
-        {            
-            //Ajax Load data from ajax
+        function pick_kmdet(id)
+        {
             $.ajax({
-                url : "<?php echo site_url('administrator/Finance/ajax_pick_kmdet/')?>/" + id,
+                url : "<?php echo site_url('administrator/Finance/ajax_pick_kmdet/')?>" + id,
                 type: "GET",
                 dataType: "JSON",
                 success: function(data)
@@ -357,29 +297,26 @@
                     var info = $('[name="kas_info"]').val();
                     var curr = $('[name="curr_name"]').val();
                     var terbi = $('[name="kas_terbilang"]').val() + ' ' + curr;
-                    // var terbi = pick_terbilang_total_km(total);
-                    for (var i = 0; i < data.length; i++) {
+                    for (var i = 0; i < data.length; i++)
+                    {
                       var $tr = $('<tr>').append(
-                            $('<td>').text(data[i]["COA_ACC"]+' - '+data[i]["COA_ACCNAME"]),
-                            $('<td>').text(data[i]["CSHINDET_INFO"]),
-                            $('<td>').css('text-align','right').text(formatCurrency(data[i]["CSHDETIN_AMOUNT"],".",",",2))
-                            // $('<td>').css('text-align','right').text(data[i]["PODET_SUB"])
+                            $('<td class="text-center">'+data[i]["COA_ACC"]+' - '+data[i]["COA_ACCNAME"]+'</td>'),
+                            $('<td class="text-center text-uppercase">'+data[i]["CSHINDET_INFO"]+'</td>'),
+                            $('<td class="text-right">'+formatCurrency(data[i]["CSHDETIN_AMOUNT"],".",",",2)+'</td>')
                             ).appendTo('#tb_content');
                     }
                     var $tr = $('<tr>').append(
-                            $('<td>').css('border-top','2px solid').text(''),
-                            $('<td>').css({'border-top':'2px solid','font-weight':'bold','text-align':'right'}).text('Total Rp'),
-                            $('<td>').css({'border-top':'2px solid','font-weight':'bold','text-align':'right'}).text(formatCurrency(total,".",",",2))
+                            $('<td></td>'),
+                            $('<th class="text-right">Total Rp</th>'),
+                            $('<th class="text-right">'+formatCurrency(total,".",",",2)+'</th>')
                             ).appendTo('#tb_content');
                     var $tr = $('<tr>').append(
-                            $('<td>').text('Keterangan :'),
-                            $('<td>').css({'font-weight':'bold','text-align':'left'}).text(info),
-                            $('<td>').css({'font-weight':'bold','text-align':'right'}).text('')
+                            $('<td class="text-center">KETERANGAN</td>'),
+                            $('<td class="text-left text-uppercase" colspan="2">'+info+'</td>')
                             ).appendTo('#tb_content');
                     var $tr = $('<tr>').append(
-                            $('<td>').text('Terbilang :'),
-                            $('<td>').css({'font-weight':'bold','text-align':'left'}).text(terbi),
-                            $('<td>').css({'font-weight':'bold','text-align':'right'}).text('')
+                            $('<td class="text-center">TERBILANG</td>'),
+                            $('<td class="text-left" colspan="2">'+terbi+'</td>')
                             ).appendTo('#tb_content');
                 },
                 error: function (jqXHR, textStatus, errorThrown)
@@ -388,8 +325,7 @@
                 }
             });
         }
-
-        function search_acc(id)
+        function pick_acc(id)
         {            
             $.ajax({
                 url : "<?php echo site_url('administrator/Finance/ajax_pick_acc/')?>" + id,
@@ -405,12 +341,10 @@
                 }
             });
         }
-
         function pick_curr(id)
         {
-            //Ajax Load data from ajax
             $.ajax({
-                url : "<?php echo site_url('administrator/Finance/ajax_pick_curr/')?>/" + id,
+                url : "<?php echo site_url('administrator/Finance/ajax_pick_curr/')?>" + id,
                 type: "GET",
                 dataType: "JSON",
                 success: function(data)
@@ -423,27 +357,41 @@
                 }
             });
         }
-
+        function pick_branch(id)
+        {
+            $.ajax({
+                url : "<?php echo site_url('administrator/Searchdata/pick_branch/')?>" + id,
+                type: "GET",
+                dataType: "JSON",
+                success: function(data)
+                {   
+                    // $('[name="img_logo"]').text(data.BRANCH_NAME);
+                    var newSrc = "<?php echo base_url()?>/assets/img/branchlogo/"+data.BRANCH_LOGO;
+                    $('#img_logo').attr('src', newSrc);
+                },
+                error: function (jqXHR, textStatus, errorThrown)
+                {
+                    alert('Error get data from ajax');
+                }
+            })
+        }
         function formatCurrency(amount, decimalSeparator, thousandsSeparator, nDecimalDigits)
         {  
             var num = parseFloat( amount ); //convert to float  
             //default values  
             decimalSeparator = decimalSeparator || '.';  
             thousandsSeparator = thousandsSeparator || ',';  
-            nDecimalDigits = nDecimalDigits == null? 2 : nDecimalDigits;  
-      
+            nDecimalDigits = nDecimalDigits == null? 2 : nDecimalDigits;
             var fixed = num.toFixed(nDecimalDigits); //limit or add decimal digits  
             //separate begin [$1], middle [$2] and decimal digits [$4]  
-            var parts = new RegExp('^(-?\\d{1,3})((?:\\d{3})+)(\\.(\\d{' + nDecimalDigits + '}))?$').exec(fixed);   
-      
+            var parts = new RegExp('^(-?\\d{1,3})((?:\\d{3})+)(\\.(\\d{' + nDecimalDigits + '}))?$').exec(fixed);
             if(parts){ //num >= 1000 || num < = -1000  
                  return parts[1] + parts[2].replace(/\d{3}/g, thousandsSeparator + '$&') + (parts[4] ? decimalSeparator + parts[4] : '');  
             }else{  
                  return fixed.replace('.', decimalSeparator);  
             }  
-        }  
+        }
     </script>
-
     <!-- print area -->
     <script type="text/javascript">
     function printContent(printpage){
